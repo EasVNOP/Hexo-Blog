c80b79deb66fa5804ca262842a169932e82b5db2
Eason Zhao <140871813+EasVNOP@users.noreply.github.com>
2024-02-18 20:19:46 +0800

    asd

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,7 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+import { defineUserConfig } from "vuepress";
+import { hopeTheme } from "vuepress-theme-hope";
 
 export default defineUserConfig({
   base: "/",
@@ -9,6 +11,8 @@ export default defineUserConfig({
   description: "来这里解放你的创造力",
 
   theme,
+  theme: hopeTheme({
+    sidebar: "heading",
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
